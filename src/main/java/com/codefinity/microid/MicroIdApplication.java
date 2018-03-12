041425c2e2fb14d6101bb757beff94dc7b04764f
Nikhil P <nikhilpat@gmail.com>
2018-03-13 00:00:00 +0530

    spring boot project created

---
+++ src/main/java/com/codefinity/microid/MicroIdApplication.java
@@ -0,0 +1,12 @@
+package com.codefinity.microid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroIdApplication.class, args);
+	}
+}
