3cf3765ca48aebe710905fc4ba405fc1fb58f625
Nikhil P <nikhilpat@gmail.com>
2018-03-21 11:23:28 +0530

    integrated config client to user microservice. Un-tested.

--- x-microservice/src/test/java/com/codefinity/microid/xservice/data/jpa/service/CityRepositoryIntegrationTests.java
+++ x-microservice/src/test/java/com/codefinity/microid/xservice/data/jpa/service/CityRepositoryIntegrationTests.java
@@ -16,37 +16,7 @@
 
 package com.codefinity.microid.xservice.data.jpa.service;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.codefinity.microcontinuum.xservice.SampleDataRestApplication;
-import com.codefinity.microid.xservice.data.jpa.domain.City;
-
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
-/**
- * Integration tests for {@link CityRepository}.
- * 
- * @author Oliver Gierke
- */
-@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = SampleDataRestApplication.class)
 public class CityRepositoryIntegrationTests {
 
-	@Autowired
-	CityRepository repository;
 
-	@Test
-	public void findsFirstPageOfCities() {
-
-		Page<City> cities = this.repository.findAll(new PageRequest(0, 10));
-		assertThat(cities.getTotalElements(), is(greaterThan(20L)));
-	}
 }
