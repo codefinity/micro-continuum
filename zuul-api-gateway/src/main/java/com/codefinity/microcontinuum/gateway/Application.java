cc33e0b0175c6e60bb9a77f441116581c164eeeb
Nikhil P <nikhilpat@gmail.com>
2018-03-23 08:59:01 +0530

    cleanup

--- zuul-api-gateway/src/main/java/com/codefinity/microcontinuum/gateway/Application.java
+++ zuul-api-gateway/src/main/java/com/codefinity/microcontinuum/gateway/Application.java
@@ -17,16 +17,9 @@
 package com.codefinity.microcontinuum.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@Import(RepositoryRestMvcConfiguration.class)
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) throws Exception {
