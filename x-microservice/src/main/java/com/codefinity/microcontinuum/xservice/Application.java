3ef3d8f2156ccbb243bc21901e5e7cb3c42c3e07
Nikhil P <nikhilpat@gmail.com>
2018-03-24 19:41:40 +0530

    updated code

--- x-microservice/src/main/java/com/codefinity/microcontinuum/xservice/Application.java
+++ x-microservice/src/main/java/com/codefinity/microcontinuum/xservice/Application.java
@@ -18,8 +18,10 @@ package com.codefinity.microcontinuum.xservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Application {
 
 	public static void main(String[] args) throws Exception {
