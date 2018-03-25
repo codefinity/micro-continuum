b50717c08c6cac31b5c92a13fbb10348d0ca3d86
Nikhil P <nikhilpat@gmail.com>
2018-03-25 19:35:06 +0530

    added eureka client annotation to microservice

--- user-microservice/src/main/java/com/codefinity/microcontinuum/user/Application.java
+++ user-microservice/src/main/java/com/codefinity/microcontinuum/user/Application.java
@@ -2,8 +2,12 @@ package com.codefinity.microcontinuum.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) throws Exception {
--- z-microservice/src/main/java/com/codefinity/microcontinuum/xservice/Application.java
+++ z-microservice/src/main/java/com/codefinity/microcontinuum/xservice/Application.java
@@ -19,9 +19,11 @@ package com.codefinity.microcontinuum.xservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) throws Exception {
