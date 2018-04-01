6822d2769e15f9c3944bc9cd00ab4c31989a5606
Nikhil P <nikhilpat@gmail.com>
2018-04-01 13:17:48 +0530

    integrated general zuul and hystrix to zuul gateway

--- zuul-api-gateway/src/main/java/com/codefinity/microcontinuum/gateway/Application.java
+++ zuul-api-gateway/src/main/java/com/codefinity/microcontinuum/gateway/Application.java
@@ -19,10 +19,14 @@ package com.codefinity.microcontinuum.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableTurbine
 @EnableZuulProxy
 public class Application {
 
